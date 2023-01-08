e1aef6a3c3290616e6bba1fbe0bc9877b63f1036
excelzed <excelzed@gmail.com>
2023-01-08 09:50:24 +0100

    init project

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -89,6 +89,65 @@ let postWebhook = (req, res) => {
     }
 };
 
+let handleMessage = async (sender_psid, received_message) => {
+    //check the incoming message is a quick reply?
+    if (received_message && received_message.quick_reply && received_message.quick_reply.payload) {
+        let payload = received_message.quick_reply.payload;
+        if (payload === "CATEGORIES") {
+            await chatbotService.sendCategories(sender_psid);
+
+        } else if (payload === "LOOKUP_ORDER") {
+            await chatbotService.sendLookupOrder(sender_psid);
+
+        } else if (payload === "TALK_AGENT") {
+            await chatbotService.requestTalkToAgent(sender_psid);
+        }
+
+        return;
+    }
+
+
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Sends the response message
+    await chatbotService.sendMessage(sender_psid, response);
+};
 
 module.exports = {
     getHomePage: getHomePage,
