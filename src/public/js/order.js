3c57b135faf5c6e12b096283b86ac76f3107fd6f
excelzed <excelzed@gmail.com>
2023-01-11 13:33:39 +0100

    update

--- src/public/js/order.js
+++ src/public/js/order.js
@@ -12,7 +12,6 @@ window.extAsyncInit = function() {
 
     MessengerExtensions.getContext(facebookAppId,
         function success(thread_context){
-            
             // success
             //set psid to input
             $("#psid").val(thread_context.psid);
@@ -24,7 +23,7 @@ window.extAsyncInit = function() {
         }
     );
 };
-console.log(facebookAppId);
+
 //validate inputs
 function validateInputFields() {
     const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
@@ -33,7 +32,6 @@ function validateInputFields() {
 
     if (!email.val().match(EMAIL_REG)) {
         email.addClass("is-invalid");
-        console.log(email);
         return true;
     } else {
         email.removeClass("is-invalid");
@@ -49,20 +47,16 @@ function validateInputFields() {
     return false;
 }
 
-
 function handleClickButtonFindOrder(){
     $("#btnFindOrder").on("click", function(e) {
         let check = validateInputFields();
-
-        
         let data = {
             psid: $("#psid").val(),
             customerName: $("#customerName").val(),
             email: $("#email").val(),
-            orderNumber: $("#orderNumber").val(),
-            
+            orderNumber: $("#orderNumber").val()
         };
-        console.log(check);
+
         if(!check) {
             //close webview
             MessengerExtensions.requestCloseBrowser(function success() {
@@ -71,7 +65,6 @@ function handleClickButtonFindOrder(){
                 // an error occurred
                 console.log(err);
             });
-            console.log(check);
 
             //send data to node.js server
             $.ajax({
@@ -88,4 +81,102 @@ function handleClickButtonFindOrder(){
         }
     });
 }
-console.log(check);
+
+
+
+
+
+
+
+
+// //load FB SDK
+// (function(d, s, id){
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) {return;}
+//     js = d.createElement(s); js.id = id;
+//     js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'Messenger'));
+
+// window.extAsyncInit = function() {
+//     // the Messenger Extensions JS SDK is done loading
+
+//     MessengerExtensions.getContext(facebookAppId,
+//         function success(thread_context){
+            
+//             // success
+//             //set psid to input
+//             $("#psid").val(thread_context.psid);
+//             handleClickButtonFindOrder();
+//         },
+//         function error(err){
+//             // error
+//             console.log(err);
+//         }
+//     );
+// };
+// console.log(facebookAppId);
+// //validate inputs
+// function validateInputFields() {
+//     const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
+//     let email = $("#email");
+//     let orderNumber = $("#orderNumber");
+
+//     if (!email.val().match(EMAIL_REG)) {
+//         email.addClass("is-invalid");
+//         console.log(email);
+//         return true;
+//     } else {
+//         email.removeClass("is-invalid");
+//     }
+
+//     if (orderNumber.val() === "") {
+//         orderNumber.addClass("is-invalid");
+//         return true;
+//     } else {
+//         orderNumber.removeClass("is-invalid");
+//     }
+
+//     return false;
+// }
+
+
+// function handleClickButtonFindOrder(){
+//     $("#btnFindOrder").on("click", function(e) {
+//         let check = validateInputFields();
+
+        
+//         let data = {
+//             psid: $("#psid").val(),
+//             customerName: $("#customerName").val(),
+//             email: $("#email").val(),
+//             orderNumber: $("#orderNumber").val(),
+            
+//         };
+//         console.log(check);
+//         if(!check) {
+//             //close webview
+//             MessengerExtensions.requestCloseBrowser(function success() {
+//                 // webview closed
+//             }, function error(err) {
+//                 // an error occurred
+//                 console.log(err);
+//             });
+//             console.log(check);
+
+//             //send data to node.js server
+//             $.ajax({
+//                 url: `${window.location.origin}/set-info-order`,
+//                 method: "POST",
+//                 data: data,
+//                 success: function(data) {
+//                     console.log(data);
+//                 },
+//                 error: function(error) {
+//                     console.log(error);
+//                 }
+//             })
+//         }
+//     });
+// }
+
