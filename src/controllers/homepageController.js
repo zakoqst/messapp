4bbb4091f4e486a8fff020076d77478229542a72
excelzed <excelzed@gmail.com>
2023-01-19 00:33:01 +0100

    update

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -172,6 +172,28 @@ let handleMessage = async (sender_psid, received_message) => {
     
 };
 
+let callSendAPI = (sender_psid, response) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
 // Handles messaging_postbacks events
 let handlePostback = async (sender_psid, received_postback) => {
     // Get the payload for the postback
