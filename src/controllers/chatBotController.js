b6baa47900471ebe3d0d3e6ebb0cd4b1f517debd
zakaria <excelzed@gmail.com>
2024-01-17 06:17:34 -0800

    Initial commit

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -41,7 +41,7 @@ let postWebhook = (req, res) =>{
 
 let getWebhook = (req, res) => {
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = process.env.MY_VERIFY_FB_TOKEN;
+    let VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
 
     // Parse the query params
     let mode = req.query['hub.mode'];
@@ -66,93 +66,90 @@ let getWebhook = (req, res) => {
 };
 
 // // Handles messages events
-// function handleMessage(sender_psid, received_message) {
-//     let response;
-
-//     // Check if the message contains text
-//     if (received_message.text) {
-
-//         // Create the payload for a basic text message
-//         response = {
-//             "text": `You sent the message: "${received_message.text}". Now send me an image!`
-//         }
-//     } else if (received_message.attachments) {
-
-//     // Gets the URL of the message attachment
-//     let attachment_url = received_message.attachments[0].payload.url;
-//         response = {
-//             "attachment": {
-//                 "type": "template",
-//                 "payload": {
-//                     "template_type": "generic",
-//                     "elements": [{
-//                         "title": "Is this the right picture?",
-//                         "subtitle": "Tap a button to answer.",
-//                         "image_url": attachment_url,
-//                         "buttons": [
-//                             {
-//                                 "type": "postback",
-//                                 "title": "Yes!",
-//                                 "payload": "yes",
-//                             },
-//                             {
-//                                 "type": "postback",
-//                                 "title": "No!",
-//                                 "payload": "no",
-//                             }
-//                         ],
-//                     }]
-//                 }
-//             }
-//         }
-
-// }
-
-// // Sends the response message
-//     callSendAPI(sender_psid, response);
-// }
+function handleMessage(sender_psid, received_message) {
+    let response;
+    
+    // Checks if the message contains text
+    if (received_message.text) {    
+      // Create the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      }
+    } else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Is this the right picture?",
+              "subtitle": "Tap a button to answer.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+    } 
+    
+    // Send the response message
+    callSendAPI(sender_psid, response);    
+  }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
-
+    
     // Get the payload for the postback
     let payload = received_postback.payload;
-
+  
     // Set the response based on the postback payload
     if (payload === 'yes') {
-        response = { "text": "Thanks!" }
+      response = { "text": "Thanks!" }
     } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
+      response = { "text": "Oops, try sending another image." }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
-}
-
+  }
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": { "text": response }
-    };
-
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v7.0/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN},
-        "method": "POST",
-        "json": request_body
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
     }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!');
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
-}
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
+  }
 
 // function firstTrait(nlp, name) {
 //     return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
