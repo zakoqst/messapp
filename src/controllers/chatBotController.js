0196cc4a6f685a0becc97248c42ceb8048641762
zakaria <excelzed@gmail.com>
2024-01-17 10:37:56 -0800

    Initial commit

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -123,12 +123,12 @@ function handlePostback(sender_psid, received_postback) {
     let response;
     let payload = received_postback.payload;
 
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" };
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." };
-    }
+   // Set the response based on the postback payload
+if (payload.toLowerCase() === 'yes') {
+    response = { "text": "Thanks!" };
+  } else if (payload.toLowerCase() === 'no') {
+    response = { "text": "Oops, try sending another image." };
+  }
 
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
