f2b848dd7e0cf6fec11a7d51d56bb43ab8c01b27
zakaria <excelzed@gmail.com>
2024-01-17 07:38:50 -0800

    Initial commit

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -65,50 +65,50 @@ let getWebhook = (req, res) => {
     }
 };
 
-// // Handles messages events
-function handleMessage(sender_psid, received_message) {
-    let response;
+// // // Handles messages events
+// function handleMessage(sender_psid, received_message) {
+//     let response;
     
-    // Checks if the message contains text
-    if (received_message.text) {    
-      // Create the payload for a basic text message, which
-      // will be added to the body of our request to the Send API
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-      }
-    } else if (received_message.attachments) {
-      // Get the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Is this the right picture?",
-              "subtitle": "Tap a button to answer.",
-              "image_url": attachment_url,
-              "buttons": [
-                {
-                  "type": "postback",
-                  "title": "Yes!",
-                  "payload": "yes",
-                },
-                {
-                  "type": "postback",
-                  "title": "No!",
-                  "payload": "no",
-                }
-              ],
-            }]
-          }
-        }
-      }
-    } 
+//     // Checks if the message contains text
+//     if (received_message.text) {    
+//       // Create the payload for a basic text message, which
+//       // will be added to the body of our request to the Send API
+//       response = {
+//         "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+//       }
+//     } else if (received_message.attachments) {
+//       // Get the URL of the message attachment
+//       let attachment_url = received_message.attachments[0].payload.url;
+//       response = {
+//         "attachment": {
+//           "type": "template",
+//           "payload": {
+//             "template_type": "generic",
+//             "elements": [{
+//               "title": "Is this the right picture?",
+//               "subtitle": "Tap a button to answer.",
+//               "image_url": attachment_url,
+//               "buttons": [
+//                 {
+//                   "type": "postback",
+//                   "title": "Yes!",
+//                   "payload": "yes",
+//                 },
+//                 {
+//                   "type": "postback",
+//                   "title": "No!",
+//                   "payload": "no",
+//                 }
+//               ],
+//             }]
+//           }
+//         }
+//       }
+//     } 
     
-    // Send the response message
-    callSendAPI(sender_psid, response);    
-  }
+//     // Send the response message
+//     callSendAPI(sender_psid, response);    
+//   }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
